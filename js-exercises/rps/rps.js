64317045ef9ed2afd2d0fa51ca72dd103e317261
arnoldzhou03 <arnoldzhou03@gmail.com>
2025-04-09 16:00:35 -0700

    Finished rps ui

--- js-exercises/rps/rps.js
+++ js-exercises/rps/rps.js
@@ -15,16 +15,15 @@ let humanScore = 0, computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     output = `The computer chose ${computerChoice}. `
-    if (humanChoice === computerChoice) {
-        output += "Tie!";
-        console.log(output);
-        return;
-    }
+    
     let computerWon = (humanChoice === "rock" && computerChoice === "paper"
         || humanChoice === "paper" && computerChoice === "scissors"
         || humanChoice === "scissors" && computerChoice === "rock");
     
-    if(computerWon) {
+    if (humanChoice === computerChoice) {
+        output += "Tie!";
+    }
+    else if(computerWon) {
         output += "Computer won!";
         computerScore++;
     }
@@ -33,9 +32,12 @@ function playRound(humanChoice, computerChoice) {
         humanScore++;
     }
 
-    console.log(output);
-}
+    const result = document.querySelector("#result");
+    result.textContent = output;
 
+    updateScoreDisplay();
+    // console.log(output);
+}
 function playGame() {
     while(humanScore < MAX_SCORE && computerScore < MAX_SCORE) {
         let humanChoice = getHumanChoice();
@@ -48,4 +50,42 @@ function playGame() {
     else if(computerScore == MAX_SCORE) console.log("Computer wins!");
 }
 
-playGame();
+function updateScoreDisplay() {
+    const score = document.querySelector("#score");
+    score.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+}
+
+function initGame() {
+    const rockBtn = document.createElement("button");
+    const paperBtn = document.createElement("button");
+    const scissorsBtn = document.createElement("button");
+
+    const result = document.createElement("div");
+    result.id = "result";
+    const score = document.createElement("div");
+    score.id = "score";
+    
+    rockBtn.textContent = "rock"
+    paperBtn.textContent = "paper"
+    scissorsBtn.textContent = "scissors"
+    rockBtn.addEventListener("click", () => onPress("rock"));
+    paperBtn.addEventListener("click", () => onPress("paper"));
+    scissorsBtn.addEventListener("click", () => onPress("scissors"));
+
+    const body = document.querySelector("body");
+    body.appendChild(rockBtn);
+    body.appendChild(paperBtn);
+    body.appendChild(scissorsBtn);
+    body.appendChild(result);
+    body.appendChild(score);
+
+    updateScoreDisplay();
+}
+
+function onPress(humanChoice) {
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+}
+
+initGame();
+// playGame();
