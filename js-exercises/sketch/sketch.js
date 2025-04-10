dcb2405796d85a61b7987090327eb4d83e37cf2d
arnoldzhou03 <arnoldzhou03@gmail.com>
2025-04-09 20:36:02 -0700

    Finished sketch

--- js-exercises/sketch/sketch.js
+++ js-exercises/sketch/sketch.js
@@ -1,9 +1,13 @@
-function init() {
+const BOARD_SIZE = 960;
+
+function initBoard(dim=16) {
     const container = document.querySelector("#container");
-    for(let i = 0; i < 16; i++) {
-        for(let j = 0; j < 16; j++) {
+    for(let i = 0; i < dim; i++) {
+        for(let j = 0; j < dim; j++) {
             let div = document.createElement("div");
             div.classList.add("cell");
+            div.style.width = `calc(100% / ${dim})`;
+            div.style.height = `calc(100% / ${dim})`;
             div.addEventListener("mouseenter", startHover);
             // div.addEventListener("mouseleave", endHover);
             container.appendChild(div);
@@ -11,15 +15,42 @@ function init() {
     }
 }
 
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+
+}
+
 function startHover(e) {
     // console.log(e.target);
     let cell = e.target;
-    cell.classList.add("hovered");
+    // cell.classList.add("hovered");
+    cell.style.backgroundColor = getRandomColor();
 }
 
 function endHover(e) {
     let cell = e.target;
     cell.classList.remove("hovered");
 }
+
+function clearBoard() {
+    const board = document.querySelector("#container");
+    board.replaceChildren();
+}
+
+function reset() {
+    clearBoard();
+    let dim = parseInt(prompt("Enter board dimension (from 0 to 100):"));
+    initBoard(dim);
+}
+
+function init() {
+    const resetBtn = document.querySelector("#reset");
+    resetBtn.addEventListener("click", reset);
+    initBoard();
+}
+
 
 init();
